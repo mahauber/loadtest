c1d2557b68ca312bb278a2d7fa5d6ddf23d473eb
Matthias Hauber <55030956+mahauber@users.noreply.github.com>
2024-07-25 14:27:36 +0200

    Update coremedia_nginx_get.js

--- coremedia_nginx_get.js
+++ coremedia_nginx_get.js
@@ -2,9 +2,7 @@ import http from 'k6/http';
 
 export let options = {
   stages: [
-        { target: 20, duration: '30s' }, // linearly go from 50 iters/s to 200 iters/s for 30s
-        { target: 50, duration: '0' }, // instantly jump to 500 iters/s
-        { target: 50, duration: '1m' }, // continue with 500 iters/s for 10 minutes
+        { target: 10, duration: '1m' }
   ],
   hosts: {
     'cae-test-coremedia.mainova.de':     '172.18.121.121'
