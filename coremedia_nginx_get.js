74afac379a094f06db2895293a291bc1c6105739
Matthias Hauber <55030956+mahauber@users.noreply.github.com>
2024-07-25 13:21:11 +0200

    Create coremedia_nginx_get.js

---
+++ coremedia_nginx_get.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+
+export let options = {
+  stages: [
+        { target: 200, duration: '30s' }, // linearly go from 50 iters/s to 200 iters/s for 30s
+        { target: 500, duration: '0' }, // instantly jump to 500 iters/s
+        { target: 500, duration: '10m' }, // continue with 500 iters/s for 10 minutes
+  ],
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: true
+}
+
+export default function () {
+    let params = { headers: { Host: "cae-test-coremedia.mainova.de" } };
+    http.get("http://nginx.nginx/de/", params);
+};
